52221233789af17456b33c58da35918b46567a2c
Md. Pappu Miahn <cse.pappu@gmail.com>
2020-10-24 22:18:40 +0600

    Remove container from content section

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-{{--        @foreach ($projects as $project)--}}
-{{--            {{ $project->name }}--}}
-{{--        @endforeach--}}
-        <router-view :projects="{{json_encode($projects)}}"></router-view>
-    </div>
+    <router-view :projects="{{json_encode($projects)}}"></router-view>
+    {{--        @foreach ($projects as $project)--}}
+    {{--            {{ $project->name }}--}}
+    {{--        @endforeach--}}
 @endsection
